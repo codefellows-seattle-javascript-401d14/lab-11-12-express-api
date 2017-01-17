c29fc755741749bb116e1ebb709a9316d5c3682c
jjron <jaren.escueta@gmail.com>
2017-01-17 15:17:40 -0800

    added tests for get all ids and delete

--- lab-jaren/test/game-router-test.js
+++ lab-jaren/test/game-router-test.js
@@ -6,7 +6,7 @@ const Game = require('../model/game.js');
 const baseURL = `http://localhost:${process.env.PORT || 3000}`;
 require('../server.js');
 
-describe('testing game router', function() {
+describe('testing game router, tests should not leave files in data', function() {
   let mockData = {
     title: 'XCOM 2',
     genre: 'strategy/tactics',
@@ -82,8 +82,62 @@ describe('testing game router', function() {
       })
       .catch(done);
     });
-    it('invalid id should return 404 status code', done => {
+    it('invalid id should return status 404', done => {
       superagent.get(`${baseURL}/api/games/42`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
+  });
+  describe('testing GET /api/games', function() {
+    before(done => {
+      superagent.post(`${baseURL}/api/games`)
+      .send(mockData)
+      .then(res => {
+        this.tempGameId = res.body.id;
+        done();
+      })
+      .catch(done);
+    });
+    after(done => {
+      Game.deleteById(this.tempGameId)
+      .then(() => done())
+      .catch(done);
+    });
+    it('should return an array containing .gitignore plus the IDs and status 200', done => {
+      superagent.get(`${baseURL}/api/games`)
+      .then(res => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.instanceof(Array);
+        expect(res.body.indexOf(this.tempGameId)).to.not.equal(-1);
+        done();
+      })
+      .catch(done);
+    });
+  });
+  describe('testing DELETE /api/games/:id', function() {
+    before(done => {
+      superagent.post(`${baseURL}/api/games`)
+      .send(mockData)
+      .then(res => {
+        this.tempGameId = res.body.id;
+        done();
+      })
+      .catch(done);
+    });
+    it('valid id should delete item and return status 204', done => {
+      superagent.delete(`${baseURL}/api/games/${this.tempGameId}`)
+      .then(res => {
+        expect(res.statusCode).to.equal(204);
+        done();
+      })
+      .catch(done);
+    });
+    it('invalid id should return status 404', done => {
+      superagent.delete(`${baseURL}/api/games/${this.tempGameId}`)
       .then(done)
       .catch(err => {
         expect(err.status).to.equal(404);
