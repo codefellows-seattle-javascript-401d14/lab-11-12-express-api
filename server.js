e4316be8ecd93546a6275da700b3b76e95013023
tommytnez <tommytnez@yahoo.com>
2017-01-17 19:31:07 -0800

    fix 500

--- server.js
+++ server.js
@@ -12,16 +12,17 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use(commentRouter);
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   debug('error middleware');
   console.error(err.message);
-  if(err.status)
+  if(err.status){
     res.status(err.status).send();
+    return;
+  }
   res.status(500).send();
-  next();
 });
 
 app.listen(process.env.PORT, () => {
   debug('starting server');
-  console.log('server up', process.env.PORT);
+  console.log('server up on port', process.env.PORT);
 });
